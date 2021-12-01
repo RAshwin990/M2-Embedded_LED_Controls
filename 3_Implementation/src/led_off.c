cba0cceec3db574928858f072b0eda7990aa3f31
RAshwin990 <81633037+RAshwin990@users.noreply.github.com>
2021-12-01 13:15:38 +0530

    Update led_off.c

--- 3_Implementation/src/led_off.c
+++ 3_Implementation/src/led_off.c
@@ -1,11 +1,11 @@
-#include "switch.h"
+#include "led_control.h"
 #define F_CPU 1000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
 
 
-void switch_off()
+void led_off()
 {
        PORTB &= (~(1<<PINB2))&(~(1<<PINB3));
 
